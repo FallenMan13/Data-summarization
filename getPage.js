904a46174bf93fa47f57023d51f26f9ce3947ac4
FallenMan13 <rosscraig9@gmail.com>
2017-03-14 09:22:21 +0000

    Command line param for source added

--- getPage.js
+++ getPage.js
@@ -8,34 +8,36 @@ module.exports.getPage = function getPage(scrollId, JSONrequest){ // Function to
     if(process.argv[9] != undefined){
       sendData(process.argv[9]);
     }
-    const rluri = read.createInterface({
-      input: process.stdin,
-      output: process.stdout
-    });
-    function uri(questionText, callback){
-      function analyzeAnswer(answer){
-        if(answer.toLowerCase().startsWith("y")){
-          rluri.close();
-          callback(answer);
+    else{
+      const rluri = read.createInterface({
+        input: process.stdin,
+        output: process.stdout
+      });
+      function uri(questionText, callback){
+        function analyzeAnswer(answer){
+          if(answer.toLowerCase().startsWith("y")){
+            rluri.close();
+            callback(answer);
+          }
+          else if(answer.toLowerCase().startsWith("n")){
+            rluri.question("Please enter the uri you would like to retrieve data from " + config["base_uri"], function(uri){
+              sendData(config["base_uri"] + uri);
+            })
+          }
         }
-        else if(answer.toLowerCase().startsWith("n")){
-          rluri.question("Please enter the uri you would like to retrieve data from " + config["base_uri"], function(uri){
-            sendData(config["base_uri"] + uri);
-          })
-        }
+        rluri.question(questionText, analyzeAnswer)
       }
-      rluri.question(questionText, analyzeAnswer)
-    }
-    uri("Would you like to use the default uri of " + config["base_uri"] + "jmeter-*? ", function(answer){
-      sendData(config["base_uri"] + "jmeter-*");
-    })
-    function sendData(uri){
-      console.log("Retrieving data");
-      JSONrequest.from = 0;
-      config.defaultRequest.uri = uri + "/_search?scroll=1m";
-      config.defaultRequest.body = JSONrequest;
-      request(config.defaultRequest, handle.handleResults); // Sends the request and calls the "resultHandling" module to work with the response
-    }
+      uri("Would you like to use the default uri of " + config["base_uri"] + "jmeter-*? ", function(answer){
+        sendData(config["base_uri"] + "jmeter-*");
+      })
+      function sendData(uri){
+        console.log("Retrieving data");
+        JSONrequest.from = 0;
+        config.defaultRequest.uri = uri + "/_search?scroll=1m";
+        config.defaultRequest.body = JSONrequest;
+        request(config.defaultRequest, handle.handleResults); // Sends the request and calls the "resultHandling" module to work with the response
+      }
+    }    
   }
   else{ // Otherwise if the first page has been retrieved
     config.defaultRequest.uri = config["base_uri"] + "_search/scroll";
