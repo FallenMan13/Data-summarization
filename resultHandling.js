5740638624192b75ae66718ae9d7e003a4c23f43
FallenMan13 <rosscraig9@gmail.com>
2017-03-14 12:42:55 +0000

    Aggregation attempt (not tested yet)

--- resultHandling.js
+++ resultHandling.js
@@ -1,5 +1,6 @@
 // Required modules
-var ProgressBar = require("progress")
+var ProgressBar = require("progress");
+var fs = require("fs");
 const page = require("./getPage");
 const summary = require("./dataSummary");
 const index = require("./indexData");
@@ -10,7 +11,8 @@ module.exports.handleResults = function handleSearchResults(error, response, bod
     console.log("Got an error: ", JSON.stringify(body, null, 2)); // Stringify the body of the response and output it to the console
     return;
   }
-  var res = body.hits.hits;
+  var res = body.aggregations;
+  var bucketpoint = 0;
   if(!currbar){ // if the progress bar does not exist yet i.e. currbar is still null
     currbar = new ProgressBar('  Retrieving [:bar] retrieved: :current | total: :total | % complete: :percent | elapsed time: :elapseds | time remaining: :etas', { // Create a new progress bar with the following options
         complete: '#'
@@ -19,39 +21,47 @@ module.exports.handleResults = function handleSearchResults(error, response, bod
       , total: body.hits.total
     });
   }
+  currbar.tick(0);
   res.forEach(function(re){
-    if(re._source.label != undefined){ // Only push data to the array if the label is not undefined i.e only push data which exists to the array
-      resArr.push({
-        "label" : re._source.label,
-        "elapsed_time" : re._source.elapsed_time,
-        "count" : re._source.count,
-      });
-    }
+    resArr.push({
+      "label" : re.data_labels.buckets[bucketpoint].key,
+      "total_elapsed_time" : re.data_labels.buckets[bucketpoint].data_stats.sum,
+      "average_elapsed_time" : re.data_labels.buckets[bucketpoint].data_stats.avg,
+      "min_elapsed_time" : re.data_labels.buckets[bucketpoint].data_stats.min,
+      "max_elapsed_time" : re.data_labels.buckets[bucketpoint].data_stats.max,
+      "total_count" : re.data_labels.buckets[bucketpoint].doc_count,
+    });
+    bucketpoint++;
   });
-  if(currbar.curr != body.hits.total){ // If the current index of the progress bar is less than the total number of records to retrieve
+  currbar.tick(res.length);
+  /*if(currbar.curr != body.hits.total){ // If the current index of the progress bar is less than the total number of records to retrieve
     if(currbar.curr >= body.hits.total - 1000){ // If the current index of the progress bar is within 1000 of the number of records to retrieve
       currbar.tick(body.hits.total - currbar.curr) // Tick using the difference between the total number of results and the current index of the progress bar
     }
     else{
       currbar.tick(1000); // Otherwise tick with 1000
     }
-  }
-  else if(currbar.curr === body.hits.total){ // If the current index of the progress bar is exactly equal to the total number of results to retrieve
+  }*/
+  if(currbar.curr === body.hits.total){ // If the current index of the progress bar is exactly equal to the total number of results to retrieve
   } // Do nothing and move on to the final else
-  if(res.length != 0){ // If there are still more pages to retrieve
+  /*if(res.length != 0){ // If there are still more pages to retrieve
     page.getPage(body._scroll_id); // Call the "getPage" module again with the scroll id as a parameter
-  }
-  else{ // Otherwise if there are no more pages to retrieve
+  }*/
+  //else{ // Otherwise if there are no more pages to retrieve
     if(resArr.length > 0){ // If data is present within the array
       console.log("Data collection complete");
-      var groupedArr = [];
-      summary.summarizeData(resArr, groupedArr); // Send the initial array and the summary array to the "dataSummary" module to summarize and store the data from the initial array
-      console.log("Total records: " + groupedArr.length);
-      index.indexData(groupedArr); // Send the array of summary data to the "indexData" module to attempt an index operation
+      //var groupedArr = [];
+      //summary.summarizeData(resArr, groupedArr); // Send the initial array and the summary array to the "dataSummary" module to summarize and store the data from the initial array
+      console.log("Total records: " + resArr.length)//groupedArr.length);
+      fs.writeFile("Aggregated data.txt", JSON.stringify(resArr, null, 2), function(){
+        console.log("Data saved");
+      })
+      //index.indexData(groupedArr); // Send the array of summary data to the "indexData" module to attempt an index operation
+      index.indexData(resArr);
     }
     else{ // If there was no data pushed to the array
       console.log("Data collection failed, no data could be found between the specified dates. Please try again."); // Inform the user that no data could be retrieved from the entered date range
       return; // End operation of the program
     }
-  }
+  //}
 }
