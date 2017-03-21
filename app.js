3e1f6c1abfe6b02746bde7b40a163924f6a2bea7
FallenMan13 <rosscraig9@gmail.com>
2017-03-21 09:38:32 +0000

    Update app.js

--- app.js
+++ app.js
@@ -16,8 +16,10 @@ request({
 }, function(error, response, body){
   if(error || response.statusCode != 200){
     console.log(JSON.stringify(body, null, 2));
+    return;
   }
   else{
     console.log(JSON.stringify(body, null, 2));
+    return;
   }
 })
